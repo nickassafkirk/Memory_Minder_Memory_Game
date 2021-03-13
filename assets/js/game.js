29adb4a8cb626d0d0fa9236fcccde0961469dbbe
nickassafkirk <nickassafkirk@gmail.com>
2021-03-13 19:08:15 +0000

    make hide scoreboard a standalone function to enable call of function upon game restart

--- assets/js/game.js
+++ assets/js/game.js
@@ -285,11 +285,13 @@ function showScoreOnCompletion(){
       <h4>Congratulations you Won!</h4>
       <p>Your score is <strong>${finalScore}</strong></p>
     `;
-    scoreAreaRef.addEventListener("click", function(){
-        gameplayAreaRef.classList.remove("d-none");
-        scoreAreaRef.classList.remove("show-flex");
-        scoreAreaRef.classList.add("d-none");
-    })
+    scoreAreaRef.addEventListener("click", showScoreboard);
+}
+
+function hideScoreboard(){
+    gameplayAreaRef.classList.remove("d-none");
+    scoreAreaRef.classList.remove("show-flex");
+    scoreAreaRef.classList.add("d-none");
 }
     
 
