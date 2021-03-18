f0a1205ea64e1f7544b6576df4832bec8c5db982
nickassafkirk <nickassafkirk@gmail.com>
2021-03-18 21:29:05 +0000

    add condition to display helper message if first-time user has not recorded any scores yet

--- assets/js/scoreboard.js
+++ assets/js/scoreboard.js
@@ -4,15 +4,28 @@ const getTopTen = JSON.parse(window.localStorage.getItem("topTen"));
 window.onload = createLeaderboard();
 
 function createLeaderboard(){
-    let firstRow = document.querySelector("#addLeaderboard");
-    getTopTen.forEach(individualScore => {
+    let tableContainer = document.querySelector("#leaderboard");
+    let tBody = document.querySelector("#addLeaderboard");
+    if (getTopTen === null){
+        const table = document.querySelector("table");
+        table.style.display = "none";
+        let newMessage = document.createElement("div");
+        newMessage.classList.add("message");
+        newMessage.innerHTML = `
+        No scores recorded
+        <a href="index.html" class="btn btn-success">Play the game</a>
+        To log your ten best scores.
+        `
+        tableContainer.append(newMessage);
+    } else {
+         getTopTen.forEach(individualScore => {
         let eachDate = individualScore.date;
         let eachScore = individualScore.score;
         let newRow = document.createElement("tr");
         newRow.innerHTML = `<td>${eachDate}</td><td>${eachScore}</td>`
-        firstRow.append(newRow);
-    });
-
+        tBody.append(newRow);
+      });
+    }
 }
 
 function clearLeaderBoard(){
